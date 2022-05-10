409d93d737e661ea7e193b8678c8b6051d335755
Santi Jimenez <santiagojimenezisc@gmail.com>
2022-05-10 00:21:02 -0500

    INicio de página

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div class="overflow-hidden">
+      <div class="container-fluid col-xxl-12">
+        <div class="row flex-lg-nowrap align-items-center g-5">
+          <div
+            class="col-lg-12 col-xl-5 text-center text-lg-start pt-lg-5 mt-xl-4"
+          >
+            <div class="lc-block mb-4">
+              <div editable="rich">
+                <h1 class="fw-bold display-3">Farmacia Estrella</h1>
+              </div>
+            </div>
+
+            <div class="lc-block mb-5">
+              <div editable="rich">
+                <p class="rfs-8">
+                  Ofrece los mejores productos para tu salud y bienestar
+                </p>
+              </div>
+            </div>
+
+            <div class="lc-block">
+              <div editable="rich">
+                <p class="fw-bold">Página en construcción</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /lc-block -->
+      </div>
+    </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Bienvenido al sistema de  Farmacia Estrellita" />
   </div>
 </template>
